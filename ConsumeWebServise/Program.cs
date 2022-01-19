d24cd3283cf4489624db75af87bcb0080b142d44
RefaatElabd-dev <RefaatElabd7@gmail.com>
2022-01-19 19:15:09 +0200

    Add project files.

---
+++ ConsumeWebServise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using localhost = ConsumeWebServise.ServiceReference1;
+
+namespace ConsumeWebServise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.MathServiceSoapClient MyMathService = new localhost.MathServiceSoapClient();
+            Console.WriteLine("2 + 4 = {0}", MyMathService.Add(2, 4));
+            Console.ReadLine();
+        }
+    }
+}
